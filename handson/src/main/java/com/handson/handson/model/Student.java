7ebe0e03f0c861da0ff16eca007629ff21988022
bar-schtalman <barschtalman@gmail.com>
2024-09-04 16:17:27 +0300

    with spring data

---
+++ handson/src/main/java/com/handson/handson/model/Student.java
@@ -0,0 +1,46 @@
+package com.handson.handson.model;
+
+import com.handson.handson.util.Dates;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name="student")
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, updatable = false)
+    private Date createdAt = Dates.nowUTC();
+
+    @NotEmpty
+    @Length(max = 60)
+    private String fullname;
+
+    private Date birthDate;
+
+    @Min(100)
+    @Max(800)
+    private Integer satScore;
+
+    @Min(30)
+    @Max(110)
+    private Double graduationScore;
+
+    @Length(max = 20)
+    private String phone;
+
+    @Length(max = 500)
+    private String profilePicture;
+
+}
